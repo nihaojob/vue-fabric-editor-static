d121425148bceb0558a648d4748d630fcd4ed8b5
秦少卫 <nihaojob@163.com>
2023-03-31 14:41:21 +0800

    feat(freeFont): 生成css文件

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # vue-fabric-editor-static
 vue-fabric-editor的字体、模板、SVG等素材
+
+
+## 功能
+根据 https://github.com/wordshub/free-font 项目生成css与JSON文件。
---
+++ generateFreeFont.js
@@ -0,0 +1,53 @@
+
+const https = require('https');
+const { writeFile } = require('fs').promises
+
+const freeFontUrl = 'https://wordshub.github.io/free-font/js/data.js'
+https.get(freeFontUrl, (response) => {
+  let str = '';
+  response.on('data', (chunk) => {
+    str += chunk;
+  });
+  response.on('end', () => {
+
+    const data = strToData(str)
+    // 筛选可直接使用的字体
+    const list = Object.keys(data).filter(key => {
+        return data[key].download.includes('.ttf')
+    })
+    // 生成字体和json文件
+    generateCSS(list, data)
+    generateJSON(list, data)
+  });
+
+}).on("error", (error) => {
+  console.log("Error: " + error.message);
+});
+// 字符串转对象
+function strToData(str){
+    const basic = str.split('var fontsMap =')[1]
+    const jsonStr = basic.substring(0, basic.length - 1);
+    var data = eval("(" + jsonStr + ")");
+    return data
+}
+// 生成css文件
+function generateCSS(list,data){
+    let freeFont = ''
+    list.forEach(key => {
+        freeFont += `@font-face {
+            font-family: '${data[key].name}';
+            src: url('${data[key].download}');
+          }
+          `
+    })
+    writeFile('./font/free-font.css', freeFont, 'utf8')
+}
+// 生成JSON文件
+function generateJSON(list,data){
+    let newData = {}
+    list.forEach(key => {
+        newData[key] = data[key]
+        newData[key].preview = 'https://wordshub.github.io/free-font/' + newData[key].preview
+    })
+    writeFile('./font/free-font.json', JSON.stringify(newData, null, 2), 'utf8')
+}
